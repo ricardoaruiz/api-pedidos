64904c0b41cafafc40b26737ce5bc441a5c9d3c2
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-01-14 11:31:16 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/rar/cursomc/service/DBService.java
+++ src/main/java/com/rar/cursomc/service/DBService.java
@@ -26,6 +26,7 @@ import com.rar.cursomc.repository.ClienteRespository;
 import com.rar.cursomc.repository.EnderecoRespository;
 import com.rar.cursomc.repository.EstadoRepository;
 import com.rar.cursomc.repository.ItemPedidoRespository;
+import com.rar.cursomc.repository.PagamentoRespository;
 import com.rar.cursomc.repository.PedidoRespository;
 import com.rar.cursomc.repository.ProdutoRespository;
 
@@ -36,7 +37,7 @@ public class DBService {
 	private CategoriaRepository categoriaRepository;
 	
 	@Autowired
-	private ProdutoRespository produtoRespository;
+	private ProdutoRespository produtoRepository;
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
@@ -45,87 +46,212 @@ public class DBService {
 	private EstadoRepository estadoRepository;
 	
 	@Autowired
-	private ClienteRespository clienteRespository;
+	private ClienteRespository clienteRepository;
 	
 	@Autowired
-	private EnderecoRespository enderecoRespository;
+	private EnderecoRespository enderecoRepository;
 	
 	@Autowired
-	private PedidoRespository pedidoRespository;
+	private PedidoRespository pedidoRepository;
 
 	@Autowired
-	private ItemPedidoRespository itemPedidoRespository;
+	private ItemPedidoRespository itemPedidoRepository;
 	
-	public void instantiateTestDatabase() throws ParseException {
-		Categoria cat1 = new Categoria("Informática");
-		Categoria cat2 = new Categoria("Escritório");
-		Categoria cat3 = new Categoria("Cama mesa e banho");
-		Categoria cat4 = new Categoria("Eletrônicos");
-		Categoria cat5 = new Categoria("Jardinagem");
-		Categoria cat6 = new Categoria("Decoração");
-		Categoria cat7 = new Categoria("Perfumaria");
-		Categoria cat8 = new Categoria("Fitness");
-		Categoria cat9 = new Categoria("Geek");
-		Categoria cat10 = new Categoria("Diversos");
+	@Autowired
+	private PagamentoRespository pagamentoRepository;
+	
+public void instantiateTestDatabase() throws ParseException {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto("Computador", 2000.00);
-		Produto p2 = new Produto("Impressora", 300.00);
+		Produto p2 = new Produto("Impressora", 800.00);
 		Produto p3 = new Produto("Mouse", 80.00);
+		Produto p4 = new Produto("Mesa de escritório", 300.00);
+		Produto p5 = new Produto("Toalha", 50.00);
+		Produto p6 = new Produto("Colcha", 200.00);
+		Produto p7 = new Produto("TV true color", 1200.00);
+		Produto p8 = new Produto("Roçadeira", 800.00);
+		Produto p9 = new Produto("Abajour", 100.00);
+		Produto p10 = new Produto("Pendente", 180.00);
+		Produto p11 = new Produto("Shampoo", 90.00);
 		
-		// Categorias
+		Produto p12 = new Produto("Produto 12", 10.00);
+		Produto p13 = new Produto("Produto 13", 10.00);
+		Produto p14 = new Produto("Produto 14", 10.00);
+		Produto p15 = new Produto("Produto 15", 10.00);
+		Produto p16 = new Produto("Produto 16", 10.00);
+		Produto p17 = new Produto("Produto 17", 10.00);
+		Produto p18 = new Produto("Produto 18", 10.00);
+		Produto p19 = new Produto("Produto 19", 10.00);
+		Produto p20 = new Produto("Produto 20", 10.00);
+		Produto p21 = new Produto("Produto 21", 10.00);
+		Produto p22 = new Produto("Produto 22", 10.00);
+		Produto p23 = new Produto("Produto 23", 10.00);
+		Produto p24 = new Produto("Produto 24", 10.00);
+		Produto p25 = new Produto("Produto 25", 10.00);
+		Produto p26 = new Produto("Produto 26", 10.00);
+		Produto p27 = new Produto("Produto 27", 10.00);
+		Produto p28 = new Produto("Produto 28", 10.00);
+		Produto p29 = new Produto("Produto 29", 10.00);
+		Produto p30 = new Produto("Produto 30", 10.00);
+		Produto p31 = new Produto("Produto 31", 10.00);
+		Produto p32 = new Produto("Produto 32", 10.00);
+		Produto p33 = new Produto("Produto 33", 10.00);
+		Produto p34 = new Produto("Produto 34", 10.00);
+		Produto p35 = new Produto("Produto 35", 10.00);
+		Produto p36 = new Produto("Produto 36", 10.00);
+		Produto p37 = new Produto("Produto 37", 10.00);
+		Produto p38 = new Produto("Produto 38", 10.00);
+		Produto p39 = new Produto("Produto 39", 10.00);
+		Produto p40 = new Produto("Produto 40", 10.00);
+		Produto p41 = new Produto("Produto 41", 10.00);
+		Produto p42 = new Produto("Produto 42", 10.00);
+		Produto p43 = new Produto("Produto 43", 10.00);
+		Produto p44 = new Produto("Produto 44", 10.00);
+		Produto p45 = new Produto("Produto 45", 10.00);
+		Produto p46 = new Produto("Produto 46", 10.00);
+		Produto p47 = new Produto("Produto 47", 10.00);
+		Produto p48 = new Produto("Produto 48", 10.00);
+		Produto p49 = new Produto("Produto 49", 10.00);
+		Produto p50 = new Produto("Produto 50", 10.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+		p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38,
+		p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
+		
+		p12.getCategorias().add(cat1);
+		p13.getCategorias().add(cat1);
+		p14.getCategorias().add(cat1);
+		p15.getCategorias().add(cat1);
+		p16.getCategorias().add(cat1);
+		p17.getCategorias().add(cat1);
+		p18.getCategorias().add(cat1);
+		p19.getCategorias().add(cat1);
+		p20.getCategorias().add(cat1);
+		p21.getCategorias().add(cat1);
+		p22.getCategorias().add(cat1);
+		p23.getCategorias().add(cat1);
+		p24.getCategorias().add(cat1);
+		p25.getCategorias().add(cat1);
+		p26.getCategorias().add(cat1);
+		p27.getCategorias().add(cat1);
+		p28.getCategorias().add(cat1);
+		p29.getCategorias().add(cat1);
+		p30.getCategorias().add(cat1);
+		p31.getCategorias().add(cat1);
+		p32.getCategorias().add(cat1);
+		p33.getCategorias().add(cat1);
+		p34.getCategorias().add(cat1);
+		p35.getCategorias().add(cat1);
+		p36.getCategorias().add(cat1);
+		p37.getCategorias().add(cat1);
+		p38.getCategorias().add(cat1);
+		p39.getCategorias().add(cat1);
+		p40.getCategorias().add(cat1);
+		p41.getCategorias().add(cat1);
+		p42.getCategorias().add(cat1);
+		p43.getCategorias().add(cat1);
+		p44.getCategorias().add(cat1);
+		p45.getCategorias().add(cat1);
+		p46.getCategorias().add(cat1);
+		p47.getCategorias().add(cat1);
+		p48.getCategorias().add(cat1);
+		p49.getCategorias().add(cat1);
+		p50.getCategorias().add(cat1);	
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10));
+		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
+		cat3.getProdutos().addAll(Arrays.asList(p5, p6));
+		cat4.getProdutos().addAll(Arrays.asList(p1, p2, p3, p7));
+		cat5.getProdutos().addAll(Arrays.asList(p8));
+		cat6.getProdutos().addAll(Arrays.asList(p9, p10));
+		cat7.getProdutos().addAll(Arrays.asList(p11));
 		
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
-		produtoRespository.saveAll(Arrays.asList(p1, p2, p3));
-		
-		// Estados		
-		Estado sp = new Estado("São Paulo");
-		Estado mg = new Estado("Minas Gerais");
-		estadoRepository.saveAll(Arrays.asList(sp, mg));
+		p1.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
+		p3.getCategorias().addAll(Arrays.asList(cat1, cat4));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat3));
+		p7.getCategorias().addAll(Arrays.asList(cat4));
+		p8.getCategorias().addAll(Arrays.asList(cat5));
+		p9.getCategorias().addAll(Arrays.asList(cat6));
+		p10.getCategorias().addAll(Arrays.asList(cat6));
+		p11.getCategorias().addAll(Arrays.asList(cat7));
 				
-		// Cidades
-		Cidade arturNogueira = new Cidade("Artur Nogueira", sp);
-		Cidade cosmopolis = new Cidade("Cosmópolis", sp);
-		Cidade uberlandia = new Cidade("Uberlandia", mg);
-		Cidade beloHorizonte = new Cidade("Belo Horizonte", mg);
-		cidadeRepository.saveAll(Arrays.asList(arturNogueira, cosmopolis, uberlandia, beloHorizonte));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+
+		produtoRepository.saveAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20,
+				p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38,
+				p39, p40, p41, p42, p43, p44, p45, p46, p47, p48, p49, p50));
 		
-		//Cliente e Endereco
-		Cliente c1 = new Cliente("Ricardo Ruiz", "ricardo.almendro.ruiz@gmail.com", "26852132870", TipoCliente.PESSOA_FISICA);
-		c1.getTelefones().addAll(Arrays.asList("19999412206", "1992237912"));
-		Endereco endereco1C1 = new Endereco("Alameda dos Resedas", "154", null, "Portal dos Manacás", "13162322", c1, arturNogueira);
-		Endereco endereco2C1 = new Endereco("Alameda dos Resedas1", "1541", null, "Portal dos Manacás1", "13162323", c1, cosmopolis);
-		c1.getEnderecos().addAll(Arrays.asList(endereco1C1, endereco2C1));
+		Estado est1 = new Estado("Minas Gerais");
+		Estado est2 = new Estado("São Paulo");
 		
-		this.clienteRespository.save(c1);
-		this.enderecoRespository.saveAll(Arrays.asList(endereco1C1, endereco2C1));
+		Cidade c1 = new Cidade("Uberlândia", est1);
+		Cidade c2 = new Cidade("São Paulo", est2);
+		Cidade c3 = new Cidade("Campinas", est2);
 		
-		// Pedido
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente("Maria Silva", "nelio.cursos@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente("Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOA_FISICA);
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+//		cli2.addPerfil(Perfil.ADMIN);
+		
+		Endereco e1 = new Endereco("Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco("Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco("Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
-		Pedido ped1 = new Pedido(sdf.parse("30/09/2017 10:32"), c1, c1.getEnderecos().get(0));
+		Pedido ped1 = new Pedido(sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
 		Pagamento pagto1 = new PagamentoComCartao(EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 		
-		Pedido ped2 = new Pedido(sdf.parse("10/10/2017 12:22"), c1, c1.getEnderecos().get(1));
-		Pagamento pagto2 = new PagamentoComBoleto(EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 23:59"), null);
+		Pagamento pagto2 = new PagamentoComBoleto(EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
 		ped2.setPagamento(pagto2);
 		
-		this.pedidoRespository.saveAll(Arrays.asList(ped1, ped2));
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
-		// Itens dos pedidos
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
 		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
 		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 		
 		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
 		ped2.getItens().addAll(Arrays.asList(ip3));
-
-		itemPedidoRespository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));		
 	}
 	
 }
